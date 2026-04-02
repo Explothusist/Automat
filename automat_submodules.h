9abfec8d9959e9d5144f4c40a57e21acf9f7082a
Explothusist <jcedricserio@gmail.com>
2026-04-02 12:26:54 -0500

    Syncing

--- automat_submodules.h
+++ automat_submodules.h
@@ -11,10 +11,10 @@
 #endif
 
 // Utilities
-// #define ATMT_SUBMODULE_UTILITIES_PID_CONTROLLER_ // Enables/Disables PID_Controller
+#define ATMT_SUBMODULE_UTILITIES_PID_CONTROLLER_ // Enables/Disables PID_Controller
 #define ATMT_SUBMODULE_UTILITIES_VECTOR_ // Enables/Disables Vector2, Vector3
 #define ATMT_SUBMODULE_UTILITIES_THREADSAFE_BUFFER_ // Enables/Disables ThreadsafeBuffer
-// #define ATMT_SUBMODULE_UTILITIES_KINEMATIC_PREDICTOR_ // Enables/Disables KinematicPredictor
+#define ATMT_SUBMODULE_UTILITIES_KINEMATIC_PREDICTOR_ // Enables/Disables KinematicPredictor
 
 #ifdef ATMT_SUBMODULE_HTTP_SERVER_
     // Server Platform
@@ -30,9 +30,9 @@
     #endif
 
     // Server Additional Tools
-    // #define ATMT_SUBMODULE_HTTP_SERVER_JSON_PARSING_ // Enables/Disables Parsing POST application/json type (requires bblanchon/ArduinoJson in Arduino framework)
-    // #define ATMT_SUBMODULE_HTTP_SERVER_JPEG_STREAMING_ // Enables/Disables HTMLPage_Dynamic_JPEGStreamer
-    // #define ATMT_SUBMODULE_HTTP_SERVER_CAMERA_STREAMING_SERVER_ // Enables/Disables CameraStreamingServer
+    #define ATMT_SUBMODULE_HTTP_SERVER_JSON_PARSING_ // Enables/Disables Parsing POST application/json type (requires bblanchon/ArduinoJson in Arduino framework)
+    #define ATMT_SUBMODULE_HTTP_SERVER_JPEG_STREAMING_ // Enables/Disables HTMLPage_Dynamic_JPEGStreamer
+    #define ATMT_SUBMODULE_HTTP_SERVER_CAMERA_STREAMING_SERVER_ // Enables/Disables CameraStreamingServer
     #define ATMT_SUBMODULE_HTTP_SERVER_ROBOT_DASHBOARD_SERVER_ // Enables/Disables RobotDashboardServer
 #endif
 
